edbe652470bd043b9edb229f33d04204db3791e2
moyasser20 <mohamedyasser192023@gmail.com>
2025-08-19 23:22:35 +0300

    Finish the Ui of the Login Screen

--- lib/features/auth/login/presentation/view/login_screen.dart
+++ lib/features/auth/login/presentation/view/login_screen.dart
@@ -1,10 +1,132 @@
+import 'package:flower_app/core/extensions/extensions.dart';
+import 'package:flower_app/core/l10n/translation/app_localizations.dart';
+import 'package:flower_app/core/theme/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import '../../../../../core/Widgets/Custom_Elevated_Button.dart';
+import '../../../../../core/Widgets/custom_text_field.dart';
+import '../../../../../core/extensions/validations.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    var local = AppLocalizations.of(context);
+
+    return Scaffold(
+      backgroundColor: AppColors.white,
+      body: Column(
+        children: [
+          Row(
+            children: [
+              GestureDetector(
+                  onTap: () {},
+                  child: Image.asset('assets/icons/arrow_back_icon.png')
+                      .setHorizontalAndVerticalPadding(context, 0.05, 0.07)),
+              Text(
+                local!.login,
+                style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w500,
+                    fontFamily: "Inter",
+                    fontStyle: FontStyle.normal),
+              ),
+            ],
+          ),
+          CustomTextFormField(
+              label: local.emailLabel,
+              hint: local.emailHintText,
+              validator: (value) {
+                if (value == null || value.isEmpty)
+                  return local.emailIsEmptyErrorMessage;
+                if (!Validations.validateEmail(value)) {
+                  return local.emailValidationErrorMsg;
+                }
+              }),
+          const SizedBox(
+            height: 20,
+          ),
+          CustomTextFormField(
+            label: local.passwordLabel,
+            hint: local.passwordHintText,
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return local.passwordRequiredErrorMsg;
+              }
+              if (!Validations.validatePassword(value)) {
+                return local.passwordValidationErrorMsg;
+              }
+              return null;
+            },
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          Row(
+            children: [
+              Checkbox(value: false, onChanged: (value) {}),
+              Text(
+                local.rememberMe,
+                style: TextStyle(color: AppColors.black),
+              ),
+              Spacer(),
+              TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    local.forgetPasswordTextButton,
+                    style: TextStyle(
+                        color: AppColors.black[40],
+                        decoration: TextDecoration.underline,
+                        decorationThickness: 1.5,
+                        fontSize: 15),
+                  )),
+            ],
+          ),
+          const SizedBox(
+            height: 45,
+          ),
+          CustomElevatedButton(
+            text: local.login,
+            onPressed: () {},
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          CustomElevatedButton(
+            text: local.continueAsGuestButton,
+            textColor: AppColors.grey,
+            onPressed: () {},
+            color: AppColors.white,
+            borderColor: AppColors.grey,
+          ),
+          const SizedBox(
+            height: 3,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(local.dontHaveAnAccount, style: TextStyle(
+                fontSize: 19
+              ),),
+              TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    local.signUp,
+                    style: TextStyle(
+                        color: AppColors.pink,
+                        decoration: TextDecoration.underline,
+                        decorationThickness: 1.5,
+                        decorationColor: AppColors.pink,
+                        fontSize: 19),
+                  )),
+
+            ],
+          )
+          
+        ],
+      ).setHorizontalPadding(context, 0.04),
+    );
   }
 }
