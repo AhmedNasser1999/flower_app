aa93bd61733c9eecdd8feaf038389b529aff72fa
moyasser20 <mohamedyasser192023@gmail.com>
2025-09-12 21:40:28 +0300

    Doing some changes for testing

--- lib/features/home/presentation/view/home_screen.dart
+++ lib/features/home/presentation/view/home_screen.dart
@@ -6,7 +6,13 @@ import 'package:flower_app/core/config/di.dart';
 import 'package:flower_app/core/extensions/extensions.dart';
 import 'package:flower_app/core/routes/route_names.dart';
 import 'package:flower_app/core/Widgets/section_header.dart';
+import 'package:flutter_svg/svg.dart';
 
+import '../../../../core/contants/app_icons.dart';
+import '../../../../core/l10n/translation/app_localizations.dart';
+import '../../../auth/logout/viewmodel/logout_viewmodel.dart';
+import '../../../auth/logout/views/logout_widget.dart';
+import '../../../profile/presentation/view/widgets/menu_item_widget.dart';
 import '../viewmodel/home_cubit.dart';
 import '../viewmodel/home_state.dart';
 import 'widgets/app_logo.dart';
@@ -19,6 +25,7 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var local = AppLocalizations.of(context);
     return BlocProvider(
       create: (_) => getIt<HomeCubit>()..initializeHomeData(),
       child: BlocBuilder<HomeCubit, HomeState>(
@@ -29,7 +36,28 @@ class HomeScreen extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  const AppLogo(),
+                  Row(
+                    children: [
+                      const AppLogo(),
+                      Spacer(),
+                      GestureDetector(
+                        onTap: () {
+                          showDialog(
+                            context: context,
+                            builder: (context) => BlocProvider(
+                              create: (context) => getIt<LogoutViewModel>(),
+                              child: const LogoutDialogWidget(),
+                            ),
+                          );
+                        },
+                        child: SvgPicture.asset(
+                          AppIcons.logoutIcon,
+                          width: 24,
+                          height: 24,
+                        ),
+                      ),
+                    ],
+                  ),
                   const SizedBox(height: 10.0),
                   const OrderInfo(),
                   const SizedBox(height: 10.0),
